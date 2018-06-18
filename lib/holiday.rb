ea3a22527d0d426caa1cbaccef4c1d57ff84ec68
AyanaZaire <ayanazaire@gmail.com>
2018-06-18 23:34:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ holiday_supplies = {
     :memorial_day => ["BBQ"]
   }
 }
-  # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
